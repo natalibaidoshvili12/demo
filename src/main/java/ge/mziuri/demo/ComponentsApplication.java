ee65f34ff1d9771b399096f3dcb916e3e57083cd
natalibaidoshvili12 <nbaidoshvili12@gmail.com>
2026-04-30 21:03:45 +0400

    components

---
+++ src/main/java/ge/mziuri/demo/ComponentsApplication.java
@@ -0,0 +1,66 @@
+package ge.mziuri.demo;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ComponentsApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        VBox layout = new VBox();
+//        ComboBox<String> comboBox = new ComboBox<>();
+//        comboBox.getItems().addAll("Audi", "Mercedes", "BMW");
+//        comboBox.getItems().addAll("Mitsubishi", "Honda", "Hyundai");
+//        comboBox.getItems().addAll("Tesla", "Toyota", "Jeep");
+//        comboBox.setVisibleRowCount(4);
+//        layout.getChildren().add(comboBox);
+//        comboBox.setOnAction(event -> {
+//            System.out.println(comboBox.getValue());
+//        });
+
+        TableView table = new TableView();
+        TableColumn tableColumn1 = new TableColumn("Name");
+        tableColumn1.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn tableColumn2 = new TableColumn("Description");
+        tableColumn2.setCellValueFactory(new PropertyValueFactory<>("description"));
+        TableColumn tableColumn3 = new TableColumn("Price");
+        tableColumn3.setCellValueFactory(new PropertyValueFactory<>("price"));
+        TableColumn tableColumn4 = new TableColumn("Date");
+        tableColumn4.setCellValueFactory(new PropertyValueFactory<>("date"));
+        table.getColumns().addAll(tableColumn1, tableColumn2, tableColumn3, tableColumn4);
+        ObservableList<ObjectModel> list = FXCollections.observableArrayList();
+        list.add(new ObjectModel("Name1", "Description1", "Price1", "Date1"));
+        list.add(new ObjectModel("Name2", "Description2", "Price2", "Date2"));
+        list.add(new ObjectModel("Name3", "Description3", "Price3", "Date3"));
+        list.add(new ObjectModel("Name4", "Description4", "Price4", "Date4"));
+        list.add(new ObjectModel("Name5", "Description5", "Price5", "Date5"));
+        table.setItems(list);
+
+        layout.getChildren().addAll(table);
+
+
+        Scene scene = new Scene(layout, 320, 240);
+        stage.show();
+        stage.setTitle("Components");
+        stage.setScene(scene);
+    }
+
+    private void showError(String taskCannotBeEmpty) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);alert.setContentText(taskCannotBeEmpty);
+        alert.showAndWait();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/ge/mziuri/demo/ObjectModel.java
@@ -0,0 +1,69 @@
+package ge.mziuri.demo;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class ObjectModel {
+
+    private SimpleStringProperty name;
+    private SimpleStringProperty description;
+    private SimpleStringProperty price;
+    private SimpleStringProperty date;
+
+    public ObjectModel() {
+    }
+
+    public ObjectModel(String name, String description, String price, String date) {
+        this.name = new SimpleStringProperty(name);
+        this.date = new SimpleStringProperty(date);
+        this.description = new SimpleStringProperty(description);
+        this.price = new SimpleStringProperty(price);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getDescription() {
+        return description.get();
+    }
+
+    public SimpleStringProperty descriptionProperty() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description.set(description);
+    }
+
+    public String getPrice() {
+        return price.get();
+    }
+
+    public SimpleStringProperty priceProperty() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price.set(price);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+    public SimpleStringProperty dateProperty() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date.set(date);
+    }
+}
